3cb71d837470152fafc56bb75827fbacd925922a
Trinex <44845306+Trinex64@users.noreply.github.com>
2024-05-16 20:40:30 +0100

    added ultra compact version
    just figured this out, im so cool aint i

--- rot encryption.py
+++ rot encryption.py
@@ -15,3 +15,11 @@ text = input("Enter your message: ")
 key = input("Enter the key: ")
 print(rot_n(text, key, decode=opt=='2'))
 input()
+
+### ULTRA COMPACT VER ###
+#def rot_n(t, k, d=False):
+#    s = [(ord(c.lower()) - 96) * (-1 if d else 1) for c in k]
+#    return ''.join(chr((ord(c) - (65 if c.isupper() else 97) + s[i % len(s)]) % 26 + (65 if c.isupper() else 97)) if c.isalpha() else c for i, c in enumerate(t))
+
+#o = input("1 for encode, 2 for decode: ")
+#print(rot_n(input("Message: "), input("Key: "), d=o=='2'))
